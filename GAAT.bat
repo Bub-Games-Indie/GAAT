e00efc637d720824a37a0f908b48b47e792fef7d
Bubeboi <43781076+Bubeboi@users.noreply.github.com>
2019-10-17 10:44:54 -0400

    Update GAAT.bat

--- GAAT.bat
+++ GAAT.bat
@@ -1,5 +1,10 @@
 @echo off
 cls
-echo WORK IN PROGRESS!!
+echo Welcome to GAAT VNTEA1.0
+echo Games And Activites testing Version Not Tested Early Access 1.0
+echo Press Enter To Be Redirected To The Tests List.
 set /p holder=
+goto grtst
+
+:grtst
 exit
