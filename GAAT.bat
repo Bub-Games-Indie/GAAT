17feda96631f3bc2043e61b9fa105c2aa71feb96
Bubeboi <43781076+Bubeboi@users.noreply.github.com>
2019-10-17 11:46:35 -0400

    Update GAAT.bat

--- GAAT.bat
+++ GAAT.bat
@@ -7,4 +7,18 @@ set /p holder=
 goto grtst
 
 :grtst
-exit
+set "quiz1="
+for /F "skip=1 delims=" %%i in (tests.txt) do if not defined uname set "quiz1=%%i"
+set "quiz2="
+for /F "skip=2 delims=" %%i in (tests.txt) do if not defined uname set "quiz2=%%i"
+set "quiz3="
+for /F "skip=3 delims=" %%i in (tests.txt) do if not defined uname set "quiz3=%%i"
+goto tstpg
+
+:tstpg
+cls
+echo Available tests:
+echo (1) %quiz1%
+echo (2) %quiz2%
+echo (3) %quiz3%
+set /p quisel=
