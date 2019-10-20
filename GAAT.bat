0c2f42d7edd06f939463c777c00145c0e0bbb50d
Bubeboi <43781076+Bubeboi@users.noreply.github.com>
2019-10-20 07:40:42 -0400

    UNTESTED
    Changes:
    Added answers to the quizzes. Next update I will add multiple questions to each quiz.

--- GAAT.bat
+++ GAAT.bat
@@ -19,6 +19,12 @@ set "q2="
 for /F "skip=5 delims=" %%i in (test.txt) do if not defined q2 set "q2=%%i"
 set "q3="
 for /F "skip=6 delims=" %%i in (test.txt) do if not defined q3 set "q3=%%i"
+set "quiz1ans="
+for /F "skip=7 delims=" %%i in (test.txt) do if not defined quiz1ans set "quiz1ans=%%i"
+set "quiz2ans="
+for /F "skip=8 delims=" %%i in (test.txt) do if not defined quiz2ans set "quiz2ans=%%i"
+set "quiz3ans="
+for /F "skip=9 delims=" %%i in (test.txt) do if not defined quiz3ans set "quiz3ans=%%i"
 goto tstpg
 
 :tstpg
@@ -42,14 +48,56 @@ echo Question1
 echo %q1%
 set /p q1q1=
 
+if %q1q1% == %quiz1ans% goto q1fin else goto q1fal
+
 :quiz2pg1
 cls
 echo Question1
 echo %q2%
 set /p q2q1=
 
+if %q2q1% == %quiz2ans% goto q2fin else goto q2fal
+
 :quiz3pg1
 cls
 echo Question1
 echo %q2%
 set /p q2q1=
+
+if %q3q1% == %quiz3ans% goto q3fin else goto q3fal
+
+:q1fin
+cls
+echo You have finished the quiz "%quiz1%" with all correct answers! Way to go!
+set /p holder=
+goto tstpg
+
+:q1fal
+cls
+echo You finished the quiz "%quiz1%" with all wrong questions. Press enter to be redirected to the quiz selection page.
+set /p holder=
+goto tstpg
+
+:q2fin
+cls
+echo You have finished the quiz "%quiz2%" with all correct answers! Way to go!
+set /p holder=
+goto tstpg
+
+:q2fal
+cls
+echo You finished the quiz "%quiz2%" with all wrong questions. Press enter to be redirected to the quiz selection page.
+set /p holder=
+goto tstpg
+
+:q3fin
+cls
+echo You have finished the quiz "%quiz3%" with all correct answers! Way to go!
+set /p holder=
+goto tstpg
+
+:q3fal
+cls
+echo You finished the quiz "%quiz3%" with all wrong questions. Press enter to be redirected to the quiz selection page.
+set /p holder=
+goto tstpg
