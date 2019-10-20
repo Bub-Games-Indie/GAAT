d0b27fce34ef38ce1b78b33d059efcba02750893
Bubeboi <43781076+Bubeboi@users.noreply.github.com>
2019-10-20 07:48:21 -0400

    Update GAAT.bat

--- GAAT.bat
+++ GAAT.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
-echo Welcome to GAAT VNTEA1.0
+echo Welcome to GAAT VNTEA1.5
 echo Games And Activites testing Version Not Tested Early Access 1.0
 echo Press Enter To Be Redirected To The Tests List.
 set /p holder=
